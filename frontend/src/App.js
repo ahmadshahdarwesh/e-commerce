a763352830d223aff1cc00a2b7ff8237096434de
ahmadshahdarwesh <abadkaka@yahoo.com>
2022-11-07 11:55:55 +0100

    page one coding is started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">MAF</a>
       </header>
-      <main>List of Products</main>
+      <main>
+        <h1>Feautered Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>
+                    {" "}
+                    <strong>{product.name}</strong>
+                  </p>
+                </a>
+                <p>{product.price}</p>
+                <button>Add to the Basket</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: "Bracelets one",
+      id: "123",
+      category: "Bracelets",
+      image: "/images/p1.png",
+      price: 10,
+      stock: 100,
+      brand: "MAF",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Made of original natural stones",
+    },
+
+    {
+      name: "Bracelets one",
+      category: "Bracelets",
+      image: "/images/p1.png",
+      price: 10,
+      stock: 100,
+      brand: "MAF",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Made of original natural stones",
+    },
+
+    {
+      name: "Bracelets one",
+      category: "Bracelets",
+      image: "/images/p2.png",
+      price: 10,
+      stock: 100,
+      brand: "MAF",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Made of original natural stones",
+    },
+
+    {
+      name: "Bracelets one",
+      category: "Bracelets",
+      image: "/images/p3.png",
+      price: 10,
+      stock: 100,
+      brand: "MAF",
+      rating: 4.5,
+      numReviews: 10,
+      description: "Made of original natural stones",
+    },
+  ],
+};
+
+export default data;
