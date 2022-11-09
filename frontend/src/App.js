1dd90eb8eb8096e039add9ccc23cc1f129114454
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-11-09 23:12:56 +0100

    add Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import data from "./data";
+// import data from "./data";
+import { BrowserRouter, Link, Routes } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -13,33 +16,40 @@ function App() {
     fetschData();
   }, []);
   return (
-    <div>
-      <header>
-        <a href="/">MAF</a>
-      </header>
-      <main>
-        <h1>Feautered Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">MAF</Link>
+        </header>
+        <main>
+          <Routes>
+           <Routes path="/product/:slug" element={<ProductScreen />} />
+           <Routes path="/" element={<HomeScreen />} />
+          </Routes>
+
+          <h1>Feautered Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>
-                    {" "}
-                    <strong>{product.name}</strong>
-                  </p>
+                  <img src={product.image} alt={product.name} />
                 </a>
-                <p>{product.price}</p>
-                <button>Add to the Basket</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>
+                      {" "}
+                      <strong>{product.name}</strong>
+                    </p>
+                  </a>
+                  <p>{product.price}</p>
+                  <button>Add to the Basket</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
